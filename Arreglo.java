2549da3065f165c4e31d51eecef7396e9f35fea0
Munson33 <gmunson@unitec.edu>
2016-09-08 15:49:17 -0600

    Taller

---
+++ Arreglo.java
@@ -0,0 +1,10 @@
+
+public class Arreglo{
+	public static void main(String args[]){
+		//en esta declaracion solo nums es arreglo.
+		int nums[]= new int[10], x=5,y;
+		//en esta declaracion todos serian un arreglo.
+		char [] valors, z,w;
+		valors= new char[x];
+	}
+}
